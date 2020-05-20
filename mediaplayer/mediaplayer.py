757153fde0851e3e6a7531bd7c83f0e29d377ec4
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2020-05-20 14:29:49 +0200

    Fix time calculation.

--- mediaplayer/mediaplayer.py
+++ mediaplayer/mediaplayer.py
@@ -10,9 +10,9 @@ def hhmmss(ms):
     # s = 1000
     # m = 60000
     # h = 360000
-    h, r = divmod(ms, 36000)
-    m, r = divmod(r, 60000)
-    s, _ = divmod(r, 1000)
+    s = round(ms / 1000)
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
     return ("%d:%02d:%02d" % (h,m,s)) if h else ("%d:%02d" % (m,s))
 
 class ViewerWindow(QMainWindow):
