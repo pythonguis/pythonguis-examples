64a6cc8ae157df0885e79c13bd6d30601ad55406
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2020-05-20 14:57:29 +0200

    ...

--- mediaplayer/mediaplayer.py
+++ mediaplayer/mediaplayer.py
@@ -11,11 +11,9 @@ def hhmmss(ms):
     # s = 1000
     # m = 60000
     # h = 360000
-    print(ms)
     s = round(ms / 1000)
     m, s = divmod(s, 60)
     h, m = divmod(m, 60)
-    print(h, m, s)
     return ("%d:%02d:%02d" % (h, m, s)) if h else ("%d:%02d" % (m, s))
 
 
