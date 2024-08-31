58ef27f1ea22bfe067c2e9d6aa250ff66e845645
Leodanis Pozo Ramos <lpozor78@gmail.com>
2024-08-31 15:28:45 +0200

    Remove old file

--- pyside6/tutorials/dialogs/pyside6-dialogs_011.py
+++
@@ -1,8 +0,0 @@
-    def button_clicked(self, s):
-
-        button = QMessageBox.question(self, "Question dialog", "The longer message")
-
-        if button == QMessageBox.Yes:
-            print("Yes!")
-        else:
-            print("No!")
